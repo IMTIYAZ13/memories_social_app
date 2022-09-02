b7d15e0c3990e1b7097029ff641a740e26ed6711
IMTIYAZ13 <imtiyazali828708@gmail.com>
2022-09-02 21:20:46 +0530

    pagination added and updated routes

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Container } from '@material-ui/core';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import PostDetails from "./components/PostDetails/postdetails";
+
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 
@@ -9,14 +12,20 @@ import Home from "./components/Home/home";
 import Auth from "./components/Auth/auth";
 
 const App = () => {
+
+    const user = JSON.parse(localStorage.getItem('profile'));
     return (
         <GoogleOAuthProvider clientId="326579534717-2cjt4j4dfs7gu140tq357i0p0e8e6g1q.apps.googleusercontent.com">
             <Router>
-                <Container maxWidth='lg'>
+                <Container maxWidth='xl'>
                     <Navbar />
                     <Routes>
-                        <Route exact path='/' element={<Home/>} />
-                        <Route exact path='/auth' element={<Auth/>} />
+                        <Route path="/" element={<Navigate replace to="/posts" />} />
+                        {/* <Route path="/" element={() => <Navigate replace to ="/posts" />} /> */}
+                        <Route path="/posts" element={<Home/>} />
+                        <Route path="/posts/search" element={<Home/>} />
+                        <Route path="/posts/:id" element={<PostDetails/>} />
+                        <Route path="/auth" element={(!user ? <Auth /> : <Navigate replace to ="/posts" />)} />
                     </Routes>
                 </Container>
             </Router>
